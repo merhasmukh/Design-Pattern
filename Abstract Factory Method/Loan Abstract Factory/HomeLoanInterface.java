fe4c5bae531098971ce1a50b232e550c4defc59f
mer hasmukh <55354471+mhgn@users.noreply.github.com>
2021-01-16 13:34:36 +0530

    Delete Abstract Factory Method directory

--- Abstract Factory Method/Loan Abstract Factory/HomeLoanInterface.java
+++
@@ -1,9 +0,0 @@
-
-public interface HomeLoanInterface {
-    
-    public void setLoanAmount();
-    public double getLoanAmount();
-    public double getRateOfInterest();
-    public int setNoOfYear();
-    public String TermAndCond();
-}
